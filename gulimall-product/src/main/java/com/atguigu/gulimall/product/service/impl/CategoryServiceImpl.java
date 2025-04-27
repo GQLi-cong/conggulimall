5e7e1a0fd2542986fd75f43e88f7e313bc659565
GQLi-cong <2571875806@qq.com>
2025-04-27 22:50:06 +0800

    三级分类-递归查询树形结构

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,37 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1. 查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        // 2. 组装成父子结构
+        // 2.1 找一级分类
+        List<CategoryEntity> level1Menus = entities.stream()
+                .filter(categoryEntity -> categoryEntity.getParentCid() == 0)
+                .map((menu) -> {
+                        menu.setChildren(getChildren(menu, entities));
+                        return menu;
+                    })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+
+
+        return level1Menus;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream()
+                .filter((categoryEntity) -> categoryEntity.getParentCid().equals(root.getCatId()))
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                })
+                .sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return children;
     }
 
 }
